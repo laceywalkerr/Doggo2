e55c337456f5db64a70ec5b8065c947bf220592c
Lacey Walker <bluelarksong@gmail.com>
2020-12-10 13:44:08 -0600

    added OwnerRepository.cs

---
+++ Doggo2/Repositories/OwnerRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Doggo2.Models;
+
+namespace Doggo2.Repositories
+{
+    public class OwnerRepository
+    {
+        private readonly IConfiguration _config;
+
+        public OwnerRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        //public List<Owner> GetAllOwners()
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+
+
+        //            reader.Close();
+
+        //            return owners;
+        //        }
+        //    }
+
+        //}
+
+        //public Owner GetOwnerById(int id)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+
+        //        }
+        //    }
+        //}
+    }
+}
