ad9e5d5c3e2089e7938e67f9fdad8a741ebddfd3
Lacey Walker <bluelarksong@gmail.com>
2020-12-11 16:02:27 -0600

    added in utils

---
+++ Doggo2/Repositories/Utils/ReaderUtils.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Doggo2.Repositories.Utils
+{
+    public class ReaderUtils 
+    {
+
+        // static means you can call the method without the instance of the class
+        public static string GetNullableString(SqlDataReader reader, string columnName)
+        {
+            if (!reader.IsDBNull(reader.GetOrdinal(columnName)))
+            {
+                return reader.GetString(reader.GetOrdinal(columnName));
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+    }
+}
