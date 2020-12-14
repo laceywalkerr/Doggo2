c011c248ebab4e8c782ac724ab6b4803fdaf4131
Lacey Walker <bluelarksong@gmail.com>
2020-12-13 21:06:17 -0600

    completed dog model

--- Doggo2/Models/Dog.cs
+++ Doggo2/Models/Dog.cs
@@ -7,5 +7,12 @@ namespace Doggo2.Models
 {
     public class Dog
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+        public string Breed { get; set; }
+        public string Notes { get; set; }
+        public string ImgUrl { get; set; }
+
     }
 }
