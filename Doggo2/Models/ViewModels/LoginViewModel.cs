8d1960f5177bba52f65ad68102bf80eecad8aea6
Lacey Walker <bluelarksong@gmail.com>
2020-12-16 11:12:57 -0600

    adding login

---
+++ Doggo2/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doggo2.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+    }
+}
--- Doggo2/Startup.cs
+++ Doggo2/Startup.cs
@@ -1,4 +1,5 @@
 using Doggo2.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,6 +32,11 @@ namespace Doggo2
             services.AddTransient<IDogRepository, DogRepository>();
             services.AddTransient<INeighborhoodRepository, NeighborhoodRepository>();
             services.AddTransient<IWalkRepository, WalkRepository>();
+
+            services.AddControllersWithViews();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options => options.LoginPath = "/Owners/LogIn");
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +57,7 @@ namespace Doggo2
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
